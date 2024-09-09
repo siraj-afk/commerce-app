2be43e7b86a2988aa3d1dd45f94ce4078e051ee3
siraj-afk <sirajkarimbil805@gmail.com>
2024-09-09 12:59:21 +0530

    new

---
+++ lib/Screen1.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'Screen2.dart';
+class Screen1 extends StatefulWidget {
+  const Screen1({super.key});
+
+  @override
+  State<Screen1> createState() => _Screen1State();
+}
+
+class _Screen1State extends State<Screen1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body:
+      SafeArea(
+        child: Padding(
+          padding:  EdgeInsets.only(left: 20.0.w,top: 120.h,),
+          child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding:  EdgeInsets.only(right:80.0),
+                child: Image.asset('assets/img.png'),
+              ),
+              SizedBox(height: 30.h,),
+
+              Text('All your shopping in\none App' ,style: TextStyle(
+            color: Colors.black,
+            fontSize: 35.sp,
+            fontFamily: 'Inter',
+            fontWeight: FontWeight.w400,
+            height: 0,
+          ),),
+              SizedBox(height: 20.h,),
+              Container(
+                width: 381.w,
+                height: 100.h,
+                child: Text( 'Sell your devices the smarter, faster way for \n'
+                    'immediate cash and a cleaner conscience.Sell\n'
+                    'your devices the smarter, faster way for \n'
+                    'immediate cash and a cleaner conscience.', style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w400,
+                                 ),),
+              ),
+              SizedBox(height: 60.h,),
+              Center(
+                child: Row(
+                  children: [
+                    Container(
+                      width: 175.w,
+                      height: 45.h,
+                      decoration: ShapeDecoration(
+                        color: Color(0xFF0DA54B),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+
+                    ),
+                      child: Center(child: Text('Log In', style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w700,
+                        height: 0,
+                      ),)),
+
+                    ),
+                          SizedBox(width: 20.w,),
+                    GestureDetector(onTap: (){
+                      Navigator.of(context).push(MaterialPageRoute(builder: (_)=>Screen2()));
+                    },
+                      child: Container(
+                        width: 175.w,
+                        height: 45.h,
+                        decoration: ShapeDecoration(
+                          color: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+
+                        ),
+                        child: Center(
+                          child: Text('Sign up',style: TextStyle(
+                            color:  Color(0xFF0DA54B),
+                            fontSize: 14,
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w700,
+                            height: 0,
+                          ),),
+                        )
+                      ),
+                    ),
+
+
+                  ],
+                ),
+              )],
+          ),
+        ),
+      ),
+
+    );
+  }
+}
---
+++ lib/Screen2.dart
@@ -0,0 +1,264 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+class Screen2 extends StatefulWidget {
+  const Screen2({super.key});
+
+  @override
+  State<Screen2> createState() => _Screen2State();
+}
+
+class _Screen2State extends State<Screen2> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body:
+      SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: 428.w,
+              height: 159.h,
+              color: Color(0xFF0DA54B),
+            ),
+            SizedBox(height: 30.h,),
+            Padding(
+              padding:  EdgeInsets.only(left: 120.0.w),
+              child: Row(
+                children: [
+                  Text('Login', style: TextStyle(
+                    color: Color(0xFF0DA54B),
+                    fontSize: 16,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w500,
+                  ),),
+                  SizedBox(width: 100.w,),
+                  Text('Register', style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w500,
+                  ),),
+                ],
+              ),
+            ),
+            SizedBox(height: 10.h,),
+            Padding(
+              padding: EdgeInsets.only(left: 20.0),
+              child: Row(
+                children: [
+                  Container(width: 192.w,height: 4.h,
+                      decoration: ShapeDecoration(
+                        color: Color(0xFF0DA54B),
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+                      ),),
+                  Container(width: 192.w,height: 4.h,
+                    decoration: ShapeDecoration(
+                      color: Color(0xFFD9D9D9),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+                    ),),
+                ],
+              ),
+            ),
+            SizedBox(height: 30.h,),
+            Padding(
+              padding:  EdgeInsets.only(left: 20.0),
+              child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Sign In', style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w700,
+                    height: 0,
+                  ),),
+                  SizedBox(height: 30.h,),
+                  Text("Welcome back! Don't have an account?", style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w400,
+                    height: 0,
+                  ),),
+                  SizedBox(height: 10.h,),
+                  Text('Sign Up', style: TextStyle(
+                    color: Color(0xFF0DA54B),
+                    fontSize: 16,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w400,
+                    height: 0,
+                  ),),
+                  SizedBox(height: 50.h,),
+                  Container(
+                    width: 385.w,
+                    height: 55.h,
+                    decoration: ShapeDecoration(
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(
+                          width: 1,
+                          color: Colors.black.withOpacity(0.2199999988079071),
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Username',
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30.h,),
+                  Container(
+                    width: 385.w,
+                    height: 55.h,
+                    decoration: ShapeDecoration(
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(
+                          width: 1,
+                          color: Colors.black.withOpacity(0.2199999988079071),
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Password',
+                        suffixIcon: Icon(Icons.visibility,color: Colors.green,)
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30.h,),
+                  Container(
+                    width: 385.w,
+                    height: 55.h,
+                    decoration: ShapeDecoration(
+                      color: Colors.green,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Center(child: Text('Login',  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w700,
+                      height: 0,
+                    ), )),
+
+                  ),
+                  SizedBox(height: 20,),
+                  Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Remember Me',  style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 14,
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w400,
+                        height: 0,
+                      )),
+                      Padding(
+                        padding:  EdgeInsets.only(right: 20.0),
+                        child: Text('Forgot password?',  style: TextStyle(
+                          color: Color(0xFF0DA54B),
+                          fontSize: 14,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w400,
+                          height: 0,
+                        )
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 30.h,),
+                  Row(
+                    children: [
+                      Container(
+                        width: 150,
+                        height: 2,
+                        decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                      ),
+                      SizedBox(width: 10.w,),
+                      Text('OR',style: TextStyle(
+                        color: Color(0xFF0DA54B),
+                        fontSize: 14,
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w400,
+                        height: 0,
+                      ),),
+                      SizedBox(width: 10.w,),
+                      Container(
+                        width: 150,
+                        height: 2,
+                        decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 40.h,),
+                  Container(
+                    width: 384.w,
+                    height: 50.h,
+                    decoration: ShapeDecoration(
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(
+                          width: 1,
+                          color: Colors.black.withOpacity(0.2199999988079071),
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset('assets/gg.png',width: 27.w,height: 22.h,),
+                        Text('Log in with Google', style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 14,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w400,
+                          height: 0,
+                        ),)
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20.h,),
+                  Container(
+                    width: 384.w,
+                    height: 50.h,
+                    decoration: ShapeDecoration(
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(
+                          width: 1,
+                          color: Colors.black.withOpacity(0.2199999988079071),
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset('assets/img_2.png',width: 27.w,height: 22.h,),
+                        Text('Log in with Apple', style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 14,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w400,
+                          height: 0,
+                        ),)
+                      ],
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
+            SizedBox(height: 50.h,)
+          ],
+        ),
+      ),
+    );
+  }
+}
